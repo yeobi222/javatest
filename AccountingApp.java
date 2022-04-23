95d7b11d1b0c804246cf61cc6dc484968420167f
yeobi222 <yeobi222@gmail.com>
2022-04-23 22:36:05 +0900

    third

--- AccountingApp.java
+++ AccountingApp.java
@@ -1,18 +1,34 @@
-
+import java.util.Scanner;
 public class AccountingApp {
 
 	public static void main(String[] args) {
 
 		double valofsup = 10000.0;
+		Scanner scan = new Scanner(System.in);
+		valofsup = scan.nextDouble();
+		scan.close();		
 		double vatRate = 0.1;
-		System.out.println("value of suply: " + valofsup);
 		double vat = valofsup * vatRate;
-		System.out.println("vat: " + vat);
-		System.out.println("total: " + vat + valofsup);
 		double expenseRate = 0.3;
+		double income = valofsup - valofsup * expenseRate;
+		double divideRate1 = 0.3;
+		double divideRate2 = 0.2;
+		
+		
+
+		if(valofsup < 10000.0) {
+			divideRate1 = 0.2;
+			divideRate1 = 0.14;
+		}
+		
+		System.out.println("value of suply: " + valofsup);
+		System.out.println("vat: " + vat);
+		System.out.println("total: " + (vat + valofsup));
 		System.out.println("expense: " + valofsup * expenseRate);
-		System.out.println("income: " + (valofsup - valofsup * expenseRate));
-		System.out.println("divide: " + (valofsup - valofsup * 0.3) * vatRate);
+		
+		System.out.println("income: " + income);
+		System.out.println("dividend L1: " + income * divideRate1);
+		System.out.println("dividend L2: " + income * divideRate2);
 	}
 
 }
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # javatest
 그냥저냥 따라해보기
 
+총 공급 가액에 따라 VAT를 비롯한 여타 값을 출력하는 프로그램
+
 1. 일단 기본적으로 출력해보기
 
 2. 기본 출력문을 변수로 대체해보기
 
-* 이클립스 기능 Extract Local Variable : 원하는 부분을 자동적으로 변수로 지정. 선언하고 대체
+* 이클립스 기능 Extract Local Variable : 원하는 부분을 자동적으로 변수로 지정 선언하고 대체.
 드래그 후 우클릭-refactor-Extract Local Variable
 단축키: Alt+Shift+L
+
+3. 스캐너를 이용해 입력을 받고(클래스 import) 조건문 삽입해 출력 경우 다양화.

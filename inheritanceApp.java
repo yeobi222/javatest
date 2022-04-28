aa2487e4250eeb277cb74c0f32b334c8c9df7217
yeobi222 <yeobi222@gmail.com>
2022-04-28 18:49:22 +0900

    ninth

--- inheritanceApp.java
+++ inheritanceApp.java
@@ -1,11 +1,22 @@
 class Cal {
 	public int sum(int v1, int v2) {
+		System.out.println("cal");
 		return v1 + v2;
+	}
+	// overloading
+	public int sum(int v1, int v2, int v3) {
+		return v1 + v2 + v3;
 	}
 }
 class Cal2 extends Cal{
 	public int minus(int v1, int v2) {
 		return v1 - v2;
+	}
+	// overriding
+	
+	public int sum(int v1, int v2) {
+		System.out.println("cal2");
+		return super.sum(v1, v2);
 	}
 }
 
@@ -14,6 +25,8 @@ public class inheritanceApp {
 	public static void main(String[] args) {
 		Cal c = new Cal();
 		System.out.println(c.sum(2, 1));
+		System.out.println(c.sum(2, 1, 3));
+		
 		Cal2 c2 = new Cal2();
 		System.out.println(c2.sum(2, 2));
 		System.out.println(c2.minus(2, 1));
